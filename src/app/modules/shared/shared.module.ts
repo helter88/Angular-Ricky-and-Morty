a9130793b85d033cab2f1fc5490809b987ade78e
Artur <helter@wp.pl>
2023-01-28 17:40:30 +0100

    add card component and style

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
+import { CardComponent } from 'src/app/modules/shared/card/card.component';
 
 @NgModule({
-  declarations: [HeaderComponent, ContentComponent],
+  declarations: [HeaderComponent, ContentComponent, CardComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, ContentComponent],
+  exports: [HeaderComponent, ContentComponent, CardComponent],
 })
 export class SharedModule {}
