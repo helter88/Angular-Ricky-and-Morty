a882a6e562b409b7b8c8c51c1c5c88c0eca8d9df
Artur <helter@wp.pl>
2023-01-29 17:32:31 +0100

    add filter component with imput search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './modules/shared/shared.module';
+import { FilterComponent } from './components/filter/filter.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FilterComponent],
   imports: [BrowserModule, SharedModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
